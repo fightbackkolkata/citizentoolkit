5c29aa84845425ea60cc602928cd463b66d9f71b
fightbackkolkata <abir@lognleap.in>
2024-07-15 17:10:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const images = {
         "main": "./main.jpg",
-        "toolkit-1": "https://blog.udemy.com/wp-content/uploads/2014/05/bigstock-Vector-Promotion-Concept-Fla-57726575.jpg"
+        "toolkit-1": "https://blog.udemy.com/wp-content/uploads/2014/05/bigstock-Vector-Promotion-Concept-Fla-57726575.jpg",
+        "toolkit-2": "https://partnershiponai.org/wp-content/uploads/2022/12/local_new_tools.webp"
     };
 
     Object.keys(images).forEach(id => {
